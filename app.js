48e82c27f05c4222fc0324c5273b5d1d07b44b32
Lavinia-CristinaRus <lavinia.rus20@gmail.com>
2022-11-30 11:38:58 +0200

    trying to fix cookies

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 import express from "express";
 import User from "./routers/User.js";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 import fileUpload from "express-fileupload";
 import cors from "cors";
 
 export const app = express();
 
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+  secret:'blablabla',
+  store: store,
+  resave:true,
+  saveUninitialized:false,
+  cookie:{maxAge: 20000 * 60, sameSite: 'none', secure: false}
+}));
 app.use(
   fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
     useTempFiles: true,
   })
 );
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:19006/',
+  credentials: true, 
+  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE'],
+  exposedHeaders: ["set-cookie"]
+}));
 
 app.use("/api/v1", User);
 
